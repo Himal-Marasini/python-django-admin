d179b50e14aadc5b0d200882d0b48688e3d527b5
Himanshu-Marasini <rajshh.07@gmail.com>
2024-04-08 22:51:34 +0545

    feat: update the consumer file with socket on listener

--- websockets/consumers.py
+++ websockets/consumers.py
@@ -3,7 +3,7 @@ import re
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import StopConsumer
 from channels.layers import get_channel_layer
-
+from .socketOnEvent import SocketOnEvent
 
 
 def remove_word_and_special_characters(input_string):
@@ -20,14 +20,18 @@ class WebSocketHandlers(AsyncWebsocketConsumer):
         print("Websocket Connected...", event)
         print("Channel Name...", self.channel_name)
         
-        group_name = remove_word_and_special_characters(self.channel_name)
-        
-        await self.channel_layer.group_add(group_name,self.channel_name)
-
         await self.accept()
         
      async def websocket_receive(self, event):
       print("text_data", event)
+      
+      #         {
+#     "event_name": "loginStatus",
+#     "payload": {
+#         "test":"test"
+#     }
+# }
+        
       
       text_data = event.get("text", "")
       text_data_json = json.loads(text_data)
@@ -37,8 +41,10 @@ class WebSocketHandlers(AsyncWebsocketConsumer):
       payload = text_data_json.get("payload", {})
       
       
-      if event_name == "event1":
-         print("Handling event1")
+      if event_name == SocketOnEvent.LOGIN_STATUS.value:
+         print("Handling event", event_name)
+         await self.process_login_status(payload)
+         
          
       elif event_name == "event2":
          print("Handling event2")
@@ -47,10 +53,38 @@ class WebSocketHandlers(AsyncWebsocketConsumer):
          print(f"Unknown event: {event_name}")
     
 
-      await self.send(text_data=json.dumps({
-         'data': text_data_json
-      }))
+      # await self.send(text_data=json.dumps({
+      #    'data': text_data_json
+      # }))
 
      async def websocket_disconnect(self, close_code):
         print(f"Websocket Disconnected: {close_code}")
         raise StopConsumer()
+     
+     async def process_login_status(self, data):
+        print("process_login_status", data)
+
+        response_data = {
+            'username': "username",
+            'user_count': "user_count"
+        }
+
+        await self.send(text_data=json.dumps(response_data))
+        
+     async def response_user_count(self, data): 
+        print("Response user data...", data)
+        
+     async def delete_user_data(self, data): 
+        print("Delete user data...", data)
+
+     async def emit_from_client(self, data): 
+        print("Emit from client...", data)
+         
+     async def join(self, data): 
+        print("join...", data)
+        
+     async def force_stop_upgrade_process(self, data): 
+        print("ForceStopUpgradeProcess...", data)
+        
+     async def force_stop_config_process(self, data): 
+        print("ForceStopUpgradeProcess...", data)
